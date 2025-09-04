849b046806f265ceda449302b7ff45753e5d3bfd
Juan Pablo Méndez Valencia <juanpmendezvalencia@gmail.com>
2025-09-04 12:02:48 -0500

    feat(user): add TransactionConfig and integrate transactional support
    - Added `TransactionConfig` class for managing reactive transactions.
    - Implemented `reactiveTransactionManager(connectionFactory)`.
    - Implemented `transactionalOperator(reactiveTransactionManager)`.
    - Injected `TransactionalOperator` into `UserReactiveRepositoryAdapter`
      and applied it in `saveUser(user)`.

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/user/adapter/UserReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/user/adapter/UserReactiveRepositoryAdapter.java
@@ -9,6 +9,7 @@ import reactor.core.publisher.Mono;
 
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.reactive.TransactionalOperator;
 
 @Repository
 public class UserReactiveRepositoryAdapter extends ReactiveAdapterOperations<
@@ -17,17 +18,21 @@ public class UserReactiveRepositoryAdapter extends ReactiveAdapterOperations<
     Long,
     UserReactiveRepository
 > implements UserRepository {
-    public UserReactiveRepositoryAdapter(UserReactiveRepository repository, ObjectMapper mapper) {
+    private final TransactionalOperator transactionalOperator;
+
+    public UserReactiveRepositoryAdapter(UserReactiveRepository repository, ObjectMapper mapper, TransactionalOperator transactionalOperator) {
         /**
          *  Could be use mapper.mapBuilder if your domain model implement builder pattern
          *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
          *  Or using mapper.map with the class of the object model
          */
         super(repository, mapper, d -> mapper.map(d, User.class/* change for domain model */));
+	this.transactionalOperator = transactionalOperator;
     }
 
     @Override
     public Mono<User> saveUser(User user) {
-      return save(user);
+      return save(user)
+	.as(transactionalOperator::transactional);
     }
 }
